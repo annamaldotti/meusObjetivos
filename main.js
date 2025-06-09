7b06de7b3c3cdd665b3386982548b513cfb6144a
annaderosis <162120430+annamaldotti@users.noreply.github.com>
2025-06-09 09:03:29 -0300

    a

--- main.js
+++ main.js
@@ -16,9 +16,8 @@ for (let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05-05T00:00:00");
-let tempoAtual = new Date ();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0]. textContent = calculaTempo;
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
